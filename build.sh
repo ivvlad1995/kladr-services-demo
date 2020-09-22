1dd9890fe0f84473a9646cf3b61a423b1e269f5a
Ivanov <iv.vlad.1995@yandex.ru>
2020-09-22 13:19:23 +0400

    builder

--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@ cd dadata && mvn clean package && cd ..
 cd not-dadata && mvn clean package && cd ..
 cd gateway && mvn clean package && cd ..
 
-rm -R target/*
-cp -r src/docker/* target/
+rm -R target/
+mkdir target
 
+cp -r src/docker/* target/
 cp dadata/target/dadata-1.0.jar target/dadata/copy
 cp not-dadata/target/not-dadata-1.0.jar target/not-dadata/copy
 cp gateway/target/gateway-1.0.jar target/gateway/copy
