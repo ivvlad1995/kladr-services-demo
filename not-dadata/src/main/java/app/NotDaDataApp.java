46f166ac4f8b7066fd21c720e7443d5bb90e472d
Ivanov <iv.vlad.1995@yandex.ru>
2020-09-22 09:59:16 +0400

    edit config application yml

--- not-dadata/src/main/java/app/NotDaDataApp.java
+++ not-dadata/src/main/java/app/NotDaDataApp.java
@@ -2,10 +2,8 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class NotDaDataApp {
     public static void main(String[] args) {
         SpringApplication.run(NotDaDataApp.class, args);
