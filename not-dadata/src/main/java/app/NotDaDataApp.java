911000273573db64efbb34824a78c01e564f728c
Ivanov <iv.vlad.1995@yandex.ru>
2020-09-22 08:50:44 +0400

    add consul-all dependency

---
+++ not-dadata/src/main/java/app/NotDaDataApp.java
@@ -0,0 +1,11 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotDaDataApp {
+    public static void main(String[] args) {
+        SpringApplication.run(NotDaDataApp.class, args);
+    }
+}
