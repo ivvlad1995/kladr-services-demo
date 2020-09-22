6b7113016085baf7e163d1d31a36635a6cf3086f
Ivanov <iv.vlad.1995@yandex.ru>
2020-09-22 15:27:37 +0400

    builder

--- build.sh
+++ build.sh
@@ -10,3 +10,12 @@ cp -r src/docker/* target/
 cp dadata/target/dadata-1.0.jar target/dadata/copy
 cp not-dadata/target/not-dadata-1.0.jar target/not-dadata/copy
 cp gateway/target/gateway-1.0.jar target/gateway/copy
+
+cd target/dadata
+sudo docker build -t kladr-services:dadata . && cd ../..
+
+cd target/not-dadata
+sudo docker build -t kladr-services:not-dadata . && cd ../..
+
+cd target/gateway
+sudo docker build -t kladr-services:gateway . && cd ../..
