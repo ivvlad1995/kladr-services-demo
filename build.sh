de9ba38c92a8b0294c3df2c2d2fd473b18c64194
Ivanov <iv.vlad.1995@yandex.ru>
2020-09-23 10:34:48 +0400

    change basic image to openjdk:8-jre-alpine

--- build.sh
+++ build.sh
@@ -7,6 +7,11 @@ rm -R target/
 mkdir target
 
 cp -r src/docker/* target/
+
+mkdir target/dadata/copy
+mkdir target/not-dadata/copy
+mkdir target/gateway/copy
+
 cp dadata/target/dadata-1.0.jar target/dadata/copy
 cp not-dadata/target/not-dadata-1.0.jar target/not-dadata/copy
 cp gateway/target/gateway-1.0.jar target/gateway/copy
