4600a92c528de5790241d0b1bcb3c9c07c424486
Ivanov <iv.vlad.1995@yandex.ru>
2020-09-23 13:05:40 +0400

    configure mongodb connection, add repository, refresh controllers

--- not-dadata/src/main/java/app/controllers/MainController.java
+++ not-dadata/src/main/java/app/controllers/MainController.java
@@ -1,5 +1,8 @@
 package app.controllers;
 
+import app.models.Address;
+import app.repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/not-dadata")
 public class MainController {
+    @Autowired
+    private AddressRepository addressRepository;
+
     @RequestMapping("/")
     public String index(){
         return "Service alive and reachable";
@@ -14,11 +20,11 @@ public class MainController {
 
     @RequestMapping("/{address}")
     public Object get_address(@PathVariable String address){
-        return "GET: " + address;
+        return addressRepository.findByDescription((address.trim()).toUpperCase());
     }
 
     @RequestMapping("/add/{address}")
     public Object add_address(@PathVariable String address){
-        return "ADD: " + address;
+        return addressRepository.save(new Address((address.trim()).toUpperCase()));
     }
 }
