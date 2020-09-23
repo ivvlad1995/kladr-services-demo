f1e099ff59b905cc116d01af08e49b9685292b6b
Ivanov <iv.vlad.1995@yandex.ru>
2020-09-23 12:36:21 +0400

    add controllers get, add for not-dadata

--- not-dadata/src/main/java/app/controllers/MainController.java
+++ not-dadata/src/main/java/app/controllers/MainController.java
@@ -1,5 +1,6 @@
 package app.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,15 @@ public class MainController {
     @RequestMapping("/")
     public String index(){
         return "Service alive and reachable";
+    }
+
+    @RequestMapping("/{address}")
+    public Object get_address(@PathVariable String address){
+        return "GET: " + address;
+    }
+
+    @RequestMapping("/add/{address}")
+    public Object add_address(@PathVariable String address){
+        return "ADD: " + address;
     }
 }
